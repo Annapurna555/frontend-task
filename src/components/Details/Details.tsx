775bf891b22193b91cbc8f7d37c0dad52185ff43
Kelly <pawelkarabowicz@gmail.com>
2022-05-19 00:24:45 +0200

    Add error boundary to components

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,6 +1,8 @@
 import React, {FC, useEffect, useState} from 'react';
 import {useLocation} from "react-router-dom";
 import {AddToFavourite} from "../AddToFavourite/AddToFavourite";
+import {NavBar} from "../NavBar/NavBar";
+import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
 
 interface DetailsProps {
@@ -10,27 +12,94 @@ interface Person {
     url: string
 }
 
-const Details: FC<DetailsProps> = () => {
+interface Character {
+    name: string,
+    height: string,
+    mass: string,
+    hair_color: string,
+    skin_color: string,
+    birth_year: string
+    gender: string,
+    homeworld: string
+
+
+}
+
+export const Details: FC<DetailsProps> = () => {
     const location = useLocation()
     const state = location.state as Person
-    const {url} = state
-    const [name, setName] = useState<string | undefined>();
+    const [character, setCharacter] = useState<Character | undefined>();
+    const [place, setPlace] = useState<string | undefined>()
+    const [homeworldValue, setHomeworldValue] = useState<string | undefined>()
+    const [error, setError] = useState(false)
+    let url: string = ""
+    if (state) {
+        url = state.url
+    }
+    if (error) {
+        throw error
+    }
     useEffect(() => {
-
         (async () => {
             const response = await fetch(`${url}`)
-            const json = await response.json()
-            setName(json.name)
+            if (response.status === 200) {
+                const json = await response.json()
+                setCharacter(json)
+                setHomeworldValue(json.homeworld)
+            } else {
+                setError(true)
+            }
         })()
-    })
+    }, [])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch(`${homeworldValue}`)
+                if (response.status === 200) {
+                    const json = await response.json()
+                    setPlace(json.name)
+
+                } else {
+                    setError(true)
+                }
+            } catch (error: any) {
+                console.log(homeworldValue)
+
+            }
+        })()
+
+    }, [homeworldValue])
     return (
         <>
             <div>
-                <h2>details of {name}</h2> not implemented yet
-                {name && <AddToFavourite url={url}/>}
-
+                {
+                    character && place && <div>
+                        <NavBar/>
+                        <div>
+                            <>
+                                <h2>Name: {character.name}</h2>
+                                <h2>Height: {character.height}</h2>
+                                <h2>Mass: {character.mass}</h2>
+                                <h2>Hair Color: {character.hair_color}</h2>
+                                <h2>Skin Color: {character.skin_color}</h2>
+                                <h2>Birth of year: {character.birth_year}</h2>
+                                <h2>Gender: {character.gender}</h2>
+                                {homeworldValue && <h2>Homeworld: {place}</h2>}
+                                <AddToFavourite url={url}/>
+                            </>
+                        </div>
+                    </div>
+                }
             </div>
         </>
     );
 }
-export default Details;
+
+export const DetailsErrorBoundary = () => {
+    return (
+        <ErrorBoundary>
+            <Details/>
+        </ErrorBoundary>
+    )
+}
