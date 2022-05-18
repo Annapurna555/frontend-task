7d448eb0167172bf9920b9001ce2e55f90a21eb7
Kelly <pawelkarabowicz@gmail.com>
2022-05-19 00:23:07 +0200

    add feature delete all from favourite list

---
+++ src/actionCreators/deleteAllFromFavourite.ts
@@ -0,0 +1,3 @@
+export default function deleteAllFromFavourite() {
+    return {type: "DELETE_ALL_FROM_FAVOURITE"};
+}
--- src/components/FavouriteList/FavouriteList.tsx
+++ src/components/FavouriteList/FavouriteList.tsx
@@ -1,7 +1,9 @@
-import {FC} from "react";
+import React, {FC} from "react";
 import {useSelector} from "react-redux";
 import {RootState} from "../../app/store";
 import {DeleteFromFavourite} from "../DeleteFromFavourite/DeleteFromFavourite";
+import {NavBar} from "../NavBar/NavBar";
+import {DeleteAllFromFavourite} from "../DeleteAllFromFavourite/DeleteAllFromFavourite";
 
 interface FavouriteList {
 }
@@ -11,10 +13,12 @@ export const FavouriteList: FC<FavouriteList> = () => {
     const list: string[] = useSelector((state: RootState) => state.favouriteList)
     return (
         <div>
+            <NavBar/>
             <h1>Your favourite characters</h1>
             {list && list.map(v => {
                 return <DeleteFromFavourite url={v} key={v}/>
             })}
+            <DeleteAllFromFavourite/>
         </div>
     )
 }
