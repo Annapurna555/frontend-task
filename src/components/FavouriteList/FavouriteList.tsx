29c261026f372db7927982e17746606b40828fa0
Kelly <pawelkarabowicz@gmail.com>
2022-05-20 02:11:36 +0200

    fix: display delete all button only if characters are present

--- src/components/FavouriteList/FavouriteList.tsx
+++ src/components/FavouriteList/FavouriteList.tsx
@@ -4,21 +4,34 @@ import {RootState} from "../../app/store";
 import {DeleteFromFavourite} from "../DeleteFromFavourite/DeleteFromFavourite";
 import {NavBar} from "../NavBar/NavBar";
 import {DeleteAllFromFavourite} from "../DeleteAllFromFavourite/DeleteAllFromFavourite";
+import {Footer} from "../Footer/Footer";
 
 interface FavouriteList {
 }
 
-
 export const FavouriteList: FC<FavouriteList> = () => {
     const list: string[] = useSelector((state: RootState) => state.favouriteList)
+    const isNotEmpty = list.length !== 0
+
     return (
-        <div>
-            <NavBar/>
-            <h1>Your favourite characters</h1>
-            {list && list.map(v => {
-                return <DeleteFromFavourite url={v} key={v}/>
-            })}
-            <DeleteAllFromFavourite/>
+        <div>{
+            isNotEmpty ?
+                <>
+                    <NavBar/>
+                    <h1>Your favourite characters list</h1>
+                    <article className={"favourite-characters-list"}>
+                        {list && list.map(v => {
+                            return <DeleteFromFavourite url={v} key={v}/>
+                        })}
+                    </article>
+                    {isNotEmpty && <DeleteAllFromFavourite/>}
+                    <Footer/>
+                </> : <>
+                    <NavBar/>
+                    <h1>Your favourite list is empty</h1>
+                    <Footer/>
+                </>
+        }
         </div>
     )
 }
